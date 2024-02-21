91e20c07e40f477742aaed6d156146764e6c2a6b
HENRY ARAUJO <112120971+henrylimass@users.noreply.github.com>
2024-02-21 14:23:44 +0000

    Endpoin (get/item)

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ app.get('/oi', function (req, res) {
     res.send('Olá, Mundo')
   })
 
+  // lista de personagem
+
+  const lista = ['Rick sanchez','Morty Smith','Summer Smith']
+
+  // Read all -> get /intem
+app.get('/item',function(req, res){
+  res.send(lista)
+})
+
 app.listen(3000)
