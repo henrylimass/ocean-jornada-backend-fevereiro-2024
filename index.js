d00d10805504bd20e3ad47e7edb1e327fd9787bb
HENRY ARAUJO <112120971+henrylimass@users.noreply.github.com>
2024-02-28 14:36:51 +0000

    Configuração doenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const { MongoClient, ObjectId } = require('mongodb')
 
-const dbUrl = 'mongodb+srv://admin:JHy9QG6y9kLJItWK@cluster0.hvxo0io.mongodb.net'
+const dbUrl = process.env.DATABASE_URL
 const dbName = 'OceanJornadaBackendFev2024'
 
 async function main() {
